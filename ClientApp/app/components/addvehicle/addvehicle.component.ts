b04e647a18003eba8676a378dec73843127436e4
bishorup.banjara@outlook.com <[bishorup.banjara@gmail.com]>
2017-04-05 01:42:55 -0400

    Added Add-Vehicle Component, Menu

---
+++ ClientApp/app/components/addvehicle/addvehicle.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'add-vehicle',
+    templateUrl: 'addvehicle.component.html'
+})
+
+export class AddvehicleComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
