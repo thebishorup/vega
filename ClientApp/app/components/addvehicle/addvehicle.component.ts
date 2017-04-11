232a7f426e9e64470a757827bb733d8249498e51
bishorup.banjara@outlook.com <[bishorup.banjara@gmail.com]>
2017-04-11 00:49:13 -0400

    Fix NG-Service return type

--- ClientApp/app/components/addvehicle/addvehicle.component.ts
+++ ClientApp/app/components/addvehicle/addvehicle.component.ts
@@ -5,6 +5,7 @@ import { IModel } from './model.interface';
 import { IMake } from './make.interface';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
     selector: 'add-vehicle',
@@ -18,31 +19,41 @@ export class AddvehicleComponent implements OnInit {
     public submitted: boolean; //keeping track whether form submitted
     public event: any[] = []; //use latter to display the form changes
 
-    _make: IMake[];
-    _model: IModel[];
-    _feature: IFeature[];
+    _make;
+    _model;
+    _feature;
 
     constructor(private _vehicleService: VehicleService) {
-        this._vehicleService.getMakes()
-            .subscribe(makes => console.log(makes));
-
-        this._vehicleService.getModelsById(1)
-            .subscribe(models => console.log(models));
-
-        this._vehicleService.getFeatures()
-            .subscribe(features => console.log(features));
+        
     }
 
     ngOnInit() {
-        
-     }
+        this._vehicleService.getMakes()
+            .subscribe(makes => {
+                this._make = makes;
+                console.log("MAKES", this._make);
+            });
 
-     OnChangeMakeGetModel(id: number) {
 
-     }
+        this._vehicleService.getModelsById(1)
+            .subscribe(models => {
+                this._model = models;
+                console.log("MODELS", this._model);
+            });
 
-     public saveVehicle(isValid: boolean, f: any) 
-     {
-         console.log(f);
-     }
+
+        this._vehicleService.getFeatures()
+            .subscribe(features => {
+                this._feature = features;
+                console.log("FEATURES", this._feature);
+            });
+    }
+
+    OnChangeMakeGetModel(id: number) {
+
+    }
+
+    public saveVehicle(isValid: boolean, f: any) {
+        console.log(f);
+    }
 }
--- ClientApp/app/components/addvehicle/vehicle.service.ts
+++ ClientApp/app/components/addvehicle/vehicle.service.ts
@@ -2,50 +2,32 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Subject, Observable } from 'rxjs/Rx';
 
-import  { IMake } from './make.interface';
-import { IModel } from './model.interface';
-import { IFeature } from './feature.interface';
-
 @Injectable()
 export class VehicleService {
 
-    makes: IMake[];
-    models: IModel[];
-    features: IFeature[];
-
     constructor(private _http: Http) {
 
-     }
+    }
 
-    getMakes(): Observable<IMake[]>
-    {
+    getMakes() {
         return this._http.get("/api/makes").map(
-            (response) => {
-                return <IMake[]>response.json();
-            } 
+            response => response.json()
         ).catch(this.handleError);
     }
 
-    getModelsById(id: number): Observable<IModel[]>
-    {
+    getModelsById(id: number) {
         return this._http.get("/api/models/" + id).map(
-            (respose) => {
-                return <IModel[]>respose.json();
-            }
+            respose => respose.json()
         ).catch(this.handleError);
     }
 
-    getFeatures(): Observable<IFeature[]>
-    {
+    getFeatures() {
         return this._http.get("/api/features").map(
-            (response) => {
-                return <IFeature[]>response.json();
-            }
+            response => response.json()
         ).catch(this.handleError);
     }
 
-    private handleError(error: Response)
-    {
+    private handleError(error: Response) {
         return Observable.throw(error.statusText);
     }
 }
